3ddbc44b71a7188e504b5bd36885788799651c62
Vadim Shender <vadim@shender.org>
2017-09-03 19:39:26 +0300

    Implement wizard setup functionality

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,10 @@
 'use strict';
 
+// Keycode constants.
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+
 // Predefined data for random wizard generating.
 var WIZARD_FIRST_NAMES = [
   'Иван',
@@ -40,8 +45,19 @@ var WIZARD_EYES_COLORS = [
   'green'
 ];
 
+var FIREBALL_COLORS = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848',
+];
 
-// Generate a random int number.
+// Number of wizards to generate.
+var WIZARDS_NUMBER = 4;
+
+
+// Generate a random integer number.
 var randInt = function (n) {
   return Math.floor(n * Math.random());
 };
@@ -49,6 +65,35 @@ var randInt = function (n) {
 // Get random item from the array.
 var randChoice = function (items) {
   return items[randInt(items.length)];
+};
+
+// Get random item not equal to the given one from the array.
+var randChoiceNotEqualTo = function (items, prevValue) {
+  var newValue = prevValue;
+  while (newValue == prevValue) {
+    newValue = randChoice(items);
+  }
+  return newValue;
+};
+
+
+// Perform the RGB to hex color convertion.
+var componentToHex = function (c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+};
+
+var rgbToHex = function (rgbStr) {
+  if (rgbStr == "") {
+    return "#000000";
+  }
+
+  var componentsStr = rgbStr.match(/rgb\((.*)\)/)[1];
+  var components = componentsStr.split(/, ?/);
+  return '#' +
+    componentToHex(parseInt(components[0])) +
+    componentToHex(parseInt(components[1])) +
+    componentToHex(parseInt(components[2]));
 };
 
 
@@ -90,15 +135,11 @@ var renderWizards = function (containerBlock, wizards, wizardTemplate) {
 
 
 // Generate 4 random wizards.
-var WIZARDS_NUMBER = 4;
 var wizards = [];
 for (var i = 0; i < WIZARDS_NUMBER; i++) {
   wizards.push(generateWizardData());
 }
 
-// Find and show the settings window.
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
 
 // Find the similar characters list block and the wizard template.
 var similarListElement = document.querySelector('.setup-similar-list');
@@ -107,5 +148,94 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template').c
 // Draw the wizards.
 renderWizards(similarListElement, wizards, similarWizardTemplate);
 
-// Show the similar characters block.
-document.querySelector('.setup-similar').classList.remove('hidden');
+
+// Implement setup popup window opening and closing.
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+var userNameInput = setup.querySelector('.setup-user-name');
+
+var onPopupEscPress = function (evt) {
+  if (evt.target !== userNameInput && evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+
+// Customize name input validation.
+userNameInput.addEventListener('invalid', function (evt) {
+  var target = evt.target;
+  if (target.validity.tooShort) {
+    target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (target.validity.tooLong) {
+    target.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (target.validity.valueMissing) {
+    target.setCustomValidity('Обязательное поле');
+  } else {
+    target.setCustomValidity('');
+  }
+});
+
+// Implement minlength behavior for the Edge browser.
+userNameInput.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value.length < 2) {
+    target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else {
+    target.setCustomValidity('');
+  }
+});
+
+
+var setupWizardElement = document.querySelector('.setup-wizard');
+
+// Change wizard coat's color by click.
+var wizardCoatElement = setupWizardElement.querySelector('.wizard-coat');
+wizardCoatElement.addEventListener('click', function (evt) {
+  evt.target.style.fill = randChoiceNotEqualTo(
+    WIZARD_COAT_COLORS, evt.target.style.fill);
+});
+
+// Change wizard eyes' color by click.
+var wizardEyesElement = setupWizardElement.querySelector('.wizard-eyes');
+wizardEyesElement.addEventListener('click', function (evt) {
+  evt.target.style.fill = randChoiceNotEqualTo(
+    WIZARD_EYES_COLORS, evt.target.style.fill);
+});
+
+// Change fireboll's color by click.
+var fireballElement = document.querySelector('.setup-fireball-wrap');
+fireballElement.addEventListener('click', function (evt) {
+  evt.target.style.backgroundColor = randChoiceNotEqualTo(
+    FIREBALL_COLORS, rgbToHex(evt.target.style.backgroundColor));
+});
